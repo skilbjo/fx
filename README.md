139491e06cd3aee40aa717c6e02249d51e585450
John Skilbeck <john.skilbeck@gmail.com>
2015-11-22 03:36:25 -0200

    building api

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # fx
+https://openexchangerates.org/quick-start
+http://fixer.io/
+http://api.fixer.io/latest?base=USD
--- api.js
+++ api.js
@@ -2,26 +2,63 @@ var
 	path = require('path'),
 	fs = require('fs'),
 	req = require('request'),
-	sync = require('synchronize'),
-	sleep = require('sleep'),
+	api_key = require('./lib/key.js').OPEN_EXCHANGE_RATES_KEY
 	dir = './data',
-	h = require('./lib/helper.js')
+	h = require('./lib/helper.js'),
+	api_call = require('./lib/result.js')
 	;
 
 var api = {
-	protocol: 'http://',
+	protocol: 'https://',
 	get base () { 
-		return this.protocol+'api.fixer.io/';
+		return this.protocol+'openexchangerates.org/api/';
 	},
+	app_id: '?app_id='+api_key,
 	get latest () { 
-		return this.base +'latest/';
+		return this.base +'latest.json'+this.app_id;
 	},
-	get used () { 
-		return this.thread_url + '#p'; 
-	},
-	get image () {
-		return this.protocol+'i.4cdn.org/';
+	get currencies () {
+		return this.base+'currencies.json'+this.app_id;
 	}
+};
+
+var filter = function(data, cb){
+	var result = [];
+
+	data = data.rates;
+
+	for (var key in data) {
+		if (data.hasOwnProperty(key)){
+			result.push({
+				currency: key,
+				rate: data[key]
+			});
+		}
+	};
+
+
+	// data.map(function(item,index){
+	// 	console.log(item);
+	// });
+
+	// data_json = JSON.parse(JSON.stringify(data.rates));
+
+	// data_json.map(function(item,index){
+	// 	console.log(item);
+	// });
+
+
+	// data.map(function(item,index){
+	// 	item = item.rates;
+	// 	item.map(function(rate,index){
+	// 		result.push({
+	// 			currency: rate[index],
+	// 			rate: rate[index]
+	// 		});
+	// 	});
+	// });
+
+	cb(data);
 };
 
 var request = function(url, cb){
@@ -34,8 +71,22 @@ var request = function(url, cb){
 	});
 };
 
-var getBoards = function(cb) {
-	request(api.base + 'boards.json', function(res){
-		if (typeof(cb) == 'function') { cb(res); }		
-	});
-};
+var get_historical = function(date) {
+	return api.base+'historical/'+date+'.json'+api.app_id
+}
+
+// console.log(api_call);
+
+filter(api_call,function(data){
+	console.log(data);
+});
+
+// request(get_historical('2015-01-01'),function(data){
+// 	console.log(data);
+// });
+
+// request(get_historical('2015-01-01'),function(json){
+// 	filter(json,function(json){
+// 		console.log(json);
+// 	});
+// });
