3088bc061cedc834efe0ac359a1b8f10b1b59a71
John Skilbeck <john.skilbeck@gmail.com>
2016-11-23 23:50:00 -0800

    deploy to odroid

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var fs = require('fs'),
-	path = require('path'),
-	api = require('./api.js'),
-	h = require('./lib/helper.js')
-	;
+  path = require('path'),
+  api = require('./api.js'),
+  h = require('./lib/helper.js')
+  ;
 
 api.get_latest(function(data){
-	h.database(data,function(result){
-		console.log(data);
-	});
+  h.database(data,function(result){
+    setTimeout(function() {
+      psql.end(function(err){
+        if(err) console.log(err);
+      });
+    }, 15 * 1000);
+  });
 });
