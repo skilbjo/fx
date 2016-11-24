1749dfe2e4fce329f1e271b2b64beb88b8f7ea7a
John Skilbeck <john.skilbeck@gmail.com>
2016-11-24 00:07:36 -0800

    odroid

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 var fs = require('fs'),
   path = require('path'),
   api = require('./api.js'),
+  psql = require('.lib/database.js').psql,
   h = require('./lib/helper.js')
   ;
 
 api.get_latest(function(data){
   h.database(data,function(result){
     setTimeout(function() {
-      psql.end(function(err){
-        if(err) console.log(err);
-      });
+      psql.end();
     }, 15 * 1000);
   });
 });
